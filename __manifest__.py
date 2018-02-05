3ad7a257e252d289642c56634f35ae9b08b19d25
Indrajith <rameshinkumar627@gmail.com>
2018-02-05 10:51:13 +0530

    Initial repo settings

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -GPK*-
+
+{
+    'name': 'Surya Sarpam',
+    'version': '1.0',
+    "author": 'Yali Technologies',
+    "website": 'http://www.yalitechnologies.com/',
+    'category': 'Hospital',
+    'sequence': 15,
+    'summary': 'Hospital Management',
+    'description': 'Hospital Management',
+    'depends': ['base', 'mail'],
+    'data': [
+
+    ],
+    'demo': [
+
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
